d740d575204ec91d61416a44635e1c638eec2897
garrison <garrison@yoga720>
2018-04-14 10:44:51 -0500

    Created new main file

---
+++ LongestCommonSubsequence/src/LCD.cpp
@@ -0,0 +1,18 @@
+/*
+ * LCD.cpp
+ *
+ *  Created on: Apr 12, 2018
+ *      Author: Garrison Henkle
+ */
+
+#include "LCD.h"
+
+LCD::LCD() {
+	// TODO Auto-generated constructor stub
+
+}
+
+LCD::~LCD() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ LongestCommonSubsequence/src/LCD.h
@@ -0,0 +1,19 @@
+/*
+ * LCD.h
+ *
+ *  Created on: Apr 12, 2018
+ *      Author: Garrison Henkle
+ */
+
+#ifndef LCD_H_
+#define LCD_H_
+
+#include "Table.h";
+
+class LCD {
+public:
+	LCD();
+	virtual ~LCD();
+};
+
+#endif /* LCD_H_ */
